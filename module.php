5b4a5f93d5ca325245316b6a92ecebabdabc8c9c
SherlockJake <7thejake7@gmail.com>
2020-08-12 10:44:24 -0700

    First pass at removing scraper functionality

--- module.php
+++ module.php
@@ -19,7 +19,7 @@ class CarModule extends Module {
 	public function __construct(){
 		parent::__construct();
 		$this->routes = $this->carRoutes();
-		$this->files = array("Car.php","CarUrlParser.php","CarParserException.php","CarDB.php","CarIterator.php","CarParserStatus.php","CarCreate.php");
+		$this->files = array("CarCreate.php");
 		$this->name = "car";
 	}
 
@@ -29,34 +29,6 @@ class CarModule extends Module {
 				"callback" => "carSearchForm",
 				"Content-Type" => "text/html"
 				//"access" => true
-			),
-			"load-cars" => array(
-				"callback" => "loadCarsData",
-				"Content-Type" => "application/json"
-			),
-			"view-page" => array(
-				"callback" => "viewPage",
-				"Content-Type" => "text/html"
-			),
-			"insert-bulk-case-reviews" => array(
-				"callback" => "insertBulkCarData",
-				"Content-Type" => "text/html"
-			),
-			"insert-single-case-reviews" => array(
-				"callback" => "insertCarDataForDay",
-				"Content-Type" => "text/html"
-			),
-			"test-car-urls" => array(
-				"callback" => "getCandidateUrlOutput",
-				"Content-Type" => "application/json"
-			),
-			"car-urls" => array(
-				"callback" => "getCarUrlsByDate",
-				"Content-Type" => "application/json"
-			),
-			"car-urls-range" => array(
-				"callback" => "getUrlsRange",
-				"Content-Type" => "application/json"
 			),
 			"car-results" => array(
 				"callback" => "getCarResults",
@@ -311,30 +283,6 @@ class CarModule extends Module {
 		));
 	}
 
-	private function getSearchForm() {
-		$form = "<h2>OCDLA Criminal Apellate Review Search</h2>";
-		$form .= "<h5>Showing all results:</h5>";
-
-		$subjectSelect = $this->buildSelect("subject_1");
-		
-		$searchBox = createElement("input", ["id" => "car-search-box", "placeholder" => "Search case review"], []);
-
-		$form = createElement("form", ["id" => "car-form"], [$heading, $subjectSelect, $searchBox]);
-
-		return $form;
-	}
-
-	private function buildSelect($field) {
-		$optionStrings = $this->getListOptions($field);
-		$createOption = function($option) {
-			return createElement("option", ["value" => $option], $option);
-		};
-
-		$optionElements = array_map($createOption, $optionStrings);
-
-		return createElement("select", ["id" => "car-subject"], $optionElements);		
-	}
-
 	private function getListOptions($field) {
 		$dbResults = MysqlDatabase::query("SELECT DISTINCT {$field} FROM car ORDER BY {$field}");
 		$parsedResults = array();
@@ -380,112 +328,6 @@ class CarModule extends Module {
 		$cars = Template::renderTemplate("case-reviews",array('cases'=>$results));
 		print($cars);
 		exit;
-	}
-
-	public function insertBulkCarData($days){
-		$startTime = time();
-		$statuses = array();
-	
-		$urlDate = new DateTime();
-		for($i = 0; $i < $days; $i++){
-			$status = new CarParserStatus();
-			$status->setRuntime(time() - $startTime);
-	
-			$cars = array(); // An array of Car objects for this day.
-	
-			$urlDate->modify("-1 day");
-			//throw an exception if 2018
-			$urlDateFormat = $urlDate->format("n j Y");
-			$status->setDate($urlDate);
-			$parser = call_user_func_array("loadPage",explode(" ",$urlDateFormat));
-			$xml = $parser->getDocumentParser();
-	
-			if($xml == null){
-				$status->setMessage("not found");
-			} else {
-				try{
-					$cars = loadCarsData($xml,$parser->getSelectedUrl());
-					// This is the GLOBAL insert call.
-					if(count($cars) !== 0){
-						insert($cars);
-					}
-					//var_dump($cars);
-					$status->setMessage("everything went ok");
-					$status->setUrl($parser->getSelectedUrl());
-				} catch(DbException $e){
-					$status->setUrl($parser->getSelectedUrl());
-					$status->setMessage($e->getMessage());
-					$status->setStatusCode("DB_INSERT_ERROR");
-				} 
-			}
-			$statuses[] = $status;
-		}
-	
-	
-		Template::addPath(__DIR__ . "/templates");
-		$html = Template::renderTemplate("car-parser-statuses",array('statuses'=>$statuses));
-	
-		// ... and custom styles.
-		$css = array(
-			"active" => true,
-			"href" => "/modules/car/css/styles.css"
-		);
-		
-		//$template->addStyle($css);
-		
-	
-		return $html;
-		// displayErrors($errors);
-	}
-
-	public function insertCarDataForDay($month,$day,$year){
-		$statuses = array();
-		$urlDate = DateTime::createFromFormat ( "n j Y" , implode(" ",array($month,$day,$year)));
-	
-		$xml = loadPage($month,$day,$year);
-	
-		$status = new CarParserStatus();
-	
-		$cars = array(); // An array of Car objects for this day.
-		$urlDateFormat = $urlDate->format("n j Y");
-		$status->setDate($urlDate);
-		$parser = call_user_func_array("loadPage",explode(" ",$urlDateFormat));
-		$xml = $parser->getDocumentParser();
-	
-		if($xml == null){
-			$status->setMessage("not found");
-		} else {
-			try{
-				$cars = loadCarsData($xml,$parser->getSelectedUrl());
-				// This is the GLOBAL insert call.
-				if(count($cars) !== 0){
-					insert($cars);
-				}
-				//var_dump($cars);
-				$status->setMessage("everything went ok");
-				$status->setUrl($parser->getSelectedUrl());
-			} catch(DbException $e){
-				$status->setUrl($parser->getSelectedUrl());
-				$status->setMessage($e->getMessage());
-				$status->setStatusCode("DB_INSERT_ERROR");
-			} 
-		}
-		$statuses[] = $status;
-	
-		Template::addPath(__DIR__ . "/templates");
-		$html = Template::renderTemplate("car-parser-statuses",array('statuses'=>$statuses));
-	
-		// ... and custom styles.
-		$css = array(
-			"active" => true,
-			"href" => "/modules/car/css/styles.css"
-		);
-		
-		//$template->addStyle($css);
-		
-	
-		return $html;
-		// displayErrors($errors);
 	}
 
 	private function calculateDays($config) {
@@ -630,123 +472,4 @@ function fetchCarsFromDb($json){
 	//if results has an error returned as json
 	$results->getIterator();
 	return $results;
-}
-
-function loadPage($month,$day,$year) {
-
-	//Crate a new date formated to be passed to the CarUrlParser and pass it to the request object
-	//Create new date from numeric values only
-	$urlDate = DateTime::createFromFormat ( "n j Y" , implode(" ",array($month,$day,$year)));
-
-	$urlParser = new CarUrlParser($urlDate);
-	$urlParser->setMaxUrlTests(6);
-	$urlParser->makeRequests();
-	return $urlParser;
-
-}
-
-function viewPage($month,$day,$year){
-	$page = loadPage($month,$day,$year);
-	if($page == null){
-		throw new CarParserException("The page at that url is null");
-	}
-	return $page->saveHTML();
-}
-
-function loadCarsData($xml,$url){
-	if($xml->isDraft()){
-		return array();
-	}
-
-
-	$authorNotSubjectCount = 1;
-	//We are skipping the first to links on the page because they are links to the author and the comments
-	$linksToSkip = 2;
-	$subjects = new CarIterator($xml->getElementsByTagName("b"));
-
-	//We want to skip the element with info about the author of the case review summarizations
-	$skip = function($text){
-		return strpos($text,"Summarized") !== false; 
-	};
-
-	$subjects->setTest($skip);
-
-	$links = new CarIterator($xml->getElementsByTagName("a"));
-	$links->skip($linksToSkip);
-
-	$errors = array();
-
-	$cars = array();
-
-	foreach($subjects as $subject){
-
-		$car = new Car($subject,$links->current(),$url);	
-
-		try{
-			$car->parse();
-		}catch(CarParserException $e){
-			$errors[] = $e;
-		}
-		
-		$cars[] = $car;
-		$links->next();
-	}
-	return $cars;
-}
-
-//----------Testing Functions-----------------
-//route that takes an int number of days starting today tho attempts to load urls for without execution of calluserfunc line
-function getCandidateUrlOutput($days){
-	set_time_limit(900);
-
-	$output = array();
-
-	//output = getoutput()
-
-	//return output
-	$urlDate = new DateTime();
-	for($i = 0; $i < $days; $i++){
-		$urlDate->modify("-1 day");
-		$urlParser = new CarUrlParser($urlDate);
-		$urlParser->makeRequests();	
-		$output[] = $urlParser->getOutput();
-	}
-	return $output;
-}
-function getCarUrlsByDate($month = null,$day = null,$year = null) {
-
-	$urls = array();
-
-	$urlDate = DateTime::createFromFormat ( "n j Y" , implode(" ",array($month,$day,$year)));
-	$urlParser = new CarUrlParser($urlDate);
-
-
-	$urls = $urlParser->getUrls();
-
-
-	return $urls;
-}
-function getUrlsRange($days){
-	//gets the candidate urls for a specified number of days
-
-	$date = new DateTime();
-	$urls = array();
-
-	for($i = 0; $i <= $days; $i++){
-		$date->modify("-1 day");
-		$urlParser = new CarUrlParser($date);
-		
-
-
-		//Standard class to hold metadata for the urls  
-		$data =  new StdClass();
-		$data->date = $date->format("F j, Y");
-		$data->iterationNumber = $i;
-		$data->urls = $urlParser->getUrls();
-
-		$urls[] = $data;
-
-	}
-	
-	return $urls;
 }
