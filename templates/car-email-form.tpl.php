96a1c7d9785cdd48b9e3bd43812e8053a4d052e3
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-02-15 13:04:24 -0800

    include css file for mail-form.

--- templates/car-email-form.tpl.php
+++ templates/car-email-form.tpl.php
@@ -1,23 +1,6 @@
 
-
-<!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-<style>
-    .email-container{
-        max-width: 60%;
-        background-color: lightblue;
-        padding: 2vw;
-    }
-    input[type=date]{
-        display: inline;
-        max-width: 20%;
-    }
-    .date-group{
-        text-align:center;
-        margin-top: 5%;
-    }
-</style>
+<link rel="stylesheet" type="text/css" href="<?php print module_path(); ?>/assets/css/car-mail-form.css" />
 
 
 <div class="container email-container">
